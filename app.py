eb4a5eb675617ad726c94edee8035dd82d782603
saulbel <saulitobel@gmail.com>
2022-08-29 21:44:43 +0200

    update app

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
 from pytube import YouTube
 
-# input
 location = './'
-link = input ("Introduce the link of Youtube you wanna download: ")
-yt = YouTube(link)
+
+# input
+def getInput():
+    link = input ("Introduce the link of Youtube you wanna download or 'exit' to close the app: ")
+    if link.upper() == 'EXIT':
+        return link
+    else:
+        yt = YouTube(link)
+        return yt
+
 
 # details
-print("Title: ", yt.title)
-print("Views: ", yt.views)
-print("Length: ", yt.length)
+def getDetails(yt):
+    print("Title: ", yt.title)
+    print("Views: ", yt.views)
+    print("Length: ", yt.length)
+
 
 # choose download format
 download_format = ''
 while download_format.upper() != 'EXIT':
-    download_format = input ("Introduce 'video' for mp4, 'audio' for mp3 or 'exit' if you wanna finish downloading stuff: ")
-    if download_format.upper() == 'VIDEO':
-        print ("Downloading....")
-        ys = yt.streams.get_highest_resolution()
-        ys.download(location)
-        print ("Downdload completed")
-    elif download_format.upper() == 'AUDIO':
-        print ("Downloading....")
-        ys = yt.streams.get_audio_only()
-        ys.download(location)
-        print ("Downdload completed")
-    elif download_format.upper() == 'EXIT':
+    yt = getInput()
+    if isinstance(yt, str):
         print ("Till nex time !")
+        break
     else:
-        print ("Please, introduce the words 'video', 'audio' or 'exit'")
+        getDetails(yt)
+        download_format = input ("Introduce 'video' ,'audio', or 'exit': ")
+        if download_format.upper() == 'VIDEO':
+            print ("Downloading....")
+            ys = yt.streams.get_highest_resolution()
+            ys.download(location)
+            print ("Download completed")
+        elif download_format.upper() == 'AUDIO':
+            print ("Downloading....")
+            ys = yt.streams.get_audio_only()
+            ys.download(location)
+            print ("Download completed")
+        elif download_format.upper() == 'EXIT':
+            print ("Till nex time !")
+        else:
+            print ("Please, introduce the words 'video', 'audio', or 'exit': ")
